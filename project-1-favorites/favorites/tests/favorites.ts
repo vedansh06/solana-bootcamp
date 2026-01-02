c3cc7b93fe17ec2936c904cc9c8de1fa331cc976
Vedansh Saini <vedanshsaini06@gmail.com>
2026-01-02 10:47:35 +0000

    test(anchor): add working ts tests for favorites program

--- project-1-favorites/favorites/tests/favorites.ts
+++ project-1-favorites/favorites/tests/favorites.ts
@@ -1,16 +1,20 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { Favorites } from "../target/types/favorites";
+import BN from "bn.js";
 
 describe("favorites", () => {
-  // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
 
-  const program = anchor.workspace.favorites as Program<Favorites>;
+  const program = anchor.workspace.Favorites as Program<Favorites>;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+  it("sets favorites", async () => {
+    await program.methods
+      .setFavorites(
+        new BN(7),
+        "red",
+        ["coding", "web3"]
+      )
+      .rpc();
   });
 });
