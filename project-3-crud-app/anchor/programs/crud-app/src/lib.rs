9a37dd292b9650b352af223d11dbf9fc5850b384
Vedansh Saini <vedanshsaini06@gmail.com>
2026-01-06 14:14:18 +0530

    implement create journal entry function and define associated account structures

--- project-3-crud-app/anchor/programs/crud-app/src/lib.rs
+++ project-3-crud-app/anchor/programs/crud-app/src/lib.rs
@@ -7,4 +7,48 @@ declare_id!("Count3AcZucFDPSFBAeHkQ6AvttieKUkyJ8HiQGhQwe");
 #[program]
 pub mod crudapp {
     use super::*;
+
+    pub fn create_journal_entry(
+        ctx: Context<CreateEntry>,
+        title: String,
+        message: String,
+    ) -> Result<()> {
+        journal_entry_owner = *ctx.accounts.owner.key;
+        journal_entry.title = title;
+
+
+        
+        journal_entry.message = message;
+
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+#[instruction(title: String)]
+
+pub struct CreateEntry<'info> {
+    #[account(
+        init,
+        seeds = [title.as_bytes(), owner.key().as_ref()],
+        bump,
+        space = 8 + JournalEntryState::INIT_SPACE,
+        payer = owner,
+    )]
+    pub journal_entry: Account<'info>, JournalEntryState,
+
+    #[account(mut)]
+    pub owner: Signer<'info>
+
+    pub system_program:Program<'info, System>
+}
+    #[account]
+    #[derive(InitSpace)]
+
+    pub struct JournalEntryState {
+    pub owner: Pubkey,
+    #[max_len(50)]
+    pub title:String,
+    #[max_len(1000)]
+    pub message:String
 }
