265a74ec1d9d10606b9833af6e46e209f3b66a8a
Vedansh Saini <vedanshsaini06@gmail.com>
2026-01-13 12:40:37 +0530

    add make_offer function to handle token offers and vault transfers

--- project-5-swap-program/programs/swap-program/src/lib.rs
+++ project-5-swap-program/programs/swap-program/src/lib.rs
@@ -12,10 +12,16 @@ pub use state::*;
 declare_id!("EbnUYD4DnopGrmY2y4SGwPueNqSGEYzDzuqQ57wq1m7N");
 
 #[program]
-pub mod swap_program {
+pub mod swap {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        initialize::handler(ctx)
+    pub fn make_offer(
+        context: Context<MakeOffer>,
+        id: u64,
+        token_a_offered_amount: u64,
+        token_b_wanted_amount: u64,
+    ) -> Result<()> {
+        instructions::make_offer::send_offered_tokens_to_vault(&context, token_a_offered_amount)?;
+        instructions::make_offer::save_offer(context, id, token_b_wanted_amount)
     }
 }
