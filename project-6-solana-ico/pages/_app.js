889380478c287176802dd7b62a8463cebbf95961
Vedansh Saini <vedanshsaini06@gmail.com>
2026-01-21 21:50:50 +0530

    integrate wallet provider in _app.js

--- project-6-solana-ico/pages/_app.js
+++ project-6-solana-ico/pages/_app.js
@@ -1,12 +1,37 @@
-// pages/_app.js
-
 import "../styles/globals.css";
+import "@solana/wallet-adapter-react-ui/styles.css";
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+import {
+  PhantomWalletAdapter,
+  SolflareWalletAdapter,
+} from "@solana/wallet-adapter-wallets";
+
+import { clusterApiUrl } from "@solana/web3.js";
+import { useMemo } from "react";
+
+const network = WalletAdapterNetwork.Devnet;
 
 function MyApp({ Component, pageProps }) {
+  const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+
+  const wallets = useMemo(
+    () => [new PhantomWalletAdapter(), new SolflareWalletAdapter()],
+    [network],
+  );
+
   return (
-    <>
-      <Component {...pageProps} />
-    </>
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>
+          <Component {...pageProps} />
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
   );
 }
 
