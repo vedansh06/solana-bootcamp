0980877d030cf38167cf90c4cb8c01fe85fa737c
Vedansh Saini <vedanshsaini06@gmail.com>
2026-01-27 19:10:10 +0530

    fix import statements and add missing exports

--- project-6-solana-ico/components/index.js
+++ project-6-solana-ico/components/index.js
@@ -1,5 +1,19 @@
-import NavBar from "./NavBar";
+import Navbar from "./Navbar";
 import HeroSection from "./HeroSection";
 import Admin from "./Admin";
+import Tokenomics from "./Tokenomics";
+import Roadmap from "./Roadmap";
+import Features from "./Features";
+import FAQ from "./FAQ";
+import Footer from "./Footer";
 
-export { NavBar, HeroSection, Admin };
+export {
+  Navbar,
+  HeroSection,
+  Admin,
+  Tokenomics,
+  Roadmap,
+  Features,
+  FAQ,
+  Footer,
+};
